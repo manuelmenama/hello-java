784a376e65b1955d6f339ad0008a435bd24d1512
manuelmenama <manuel.mena090589@gmail.com>
2023-03-07 15:06:12 +0100

    added third print row

--- src/org/lessons/java/StampaSaluto.java
+++ src/org/lessons/java/StampaSaluto.java
@@ -4,6 +4,7 @@ public class StampaSaluto {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello!");
+        System.out.println("!");
 
     }
 }
